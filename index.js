17fc068e90e01b6305b1791d296cc8588ec5aeb0
Michael Kato <rawmeat3000@gmail.com>
2025-03-24 00:09:31 -0700

    adding test shader

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Project data with detailed information
 const projectData = {
     "project-perf-framework": {
@@ -357,6 +358,18 @@ const projectData = {
       ],
       video: null
     }
+/*
+    ,
+    "art-shaders": {
+      title: "Shaders",
+      description: `
+        <p>Some shaders I've made</p>
+      `,
+      images: null,
+      video: null,
+      shaders: ["<canvas id=\"canvas\" height=\"100\" width=\"100\"></canvas>"]
+    }
+*/
   };
   
   
@@ -567,7 +580,8 @@ const projectData = {
         src: item.querySelector('img').src,
         caption: item.querySelector('.art-title').textContent
       }],
-      video: null
+      video: null,
+      shaders: null
     };
     
     const processedImages = normalizeImageData(art.images);
@@ -596,7 +610,17 @@ const projectData = {
           ${image.caption ? `<div class="expanded-image-caption">${image.caption}</div>` : ''}
         </div>`;
     });
-    
+
+/*
+    if (art.shaders) {
+      art.shaders.forEach(shaderCanvas => {
+        expandedContent += `<div>
+          ${shaderCanvas}
+        </div>`;
+      })
+    };
+*/
+
     expandedContent += `</div>`;
     
     // Add video if available
@@ -870,3 +894,163 @@ const projectData = {
     setTimeout(initTechSection, 0);
     setTimeout(initDynamicBackground, 0)
   }
+
+// SHADERTOY
+"use strict";
+/**
+ * Credit is given to the following people for their work:
+ * https://github.com/gfxfundamentals/webgl2-fundamentals/graphs/contributors
+ * This is a modified version of the code from this repository:
+ * https://webgl2fundamentals.org/webgl/lessons/webgl-shadertoy.html
+ * Many thanks to the authors for their hard work.
+ */
+
+function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector("#shaderCanvas1");
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  const vs = `#version 300 es
+     // an attribute is an input (in) to a vertex shader.
+     // It will receive data from a buffer
+     in vec4 a_position;
+
+     // all shaders have a main function
+     void main() {
+
+       // gl_Position is a special variable a vertex shader
+       // is responsible for setting
+       gl_Position = a_position;
+     }
+   `;
+
+  const fs = `#version 300 es
+   precision highp float;
+
+   uniform vec2 iResolution;
+   uniform vec2 iMouse;
+   uniform float iTime;
+#define t iTime
+#define r iResolution.xy
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ){
+	vec3 c;
+	float l,z=t;
+	for(int i=0;i<3;i++) {
+		vec2 uv,p=fragCoord.xy/r;
+		uv=p;
+		p-=.5;
+		p.x*=r.x/r.y;
+		z+=.07;
+		l=length(p);
+		uv+=p/l*(sin(z)+1.)*abs(sin(l*9.-z-z));
+		c[i]=.01/length(mod(uv,1.)-.5);
+	}
+	fragColor=vec4(c/l,t);
+}
+
+   out vec4 outColor;
+
+   void main() {
+     mainImage(outColor, gl_FragCoord.xy);
+   }
+   `;
+
+  // setup GLSL program
+  const program = webglUtils.createProgramFromSources(gl, [vs, fs]);
+
+  // look up where the vertex data needs to go.
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  // look up uniform locations
+  const resolutionLocation = gl.getUniformLocation(program, "iResolution");
+  const mouseLocation = gl.getUniformLocation(program, "iMouse");
+  const timeLocation = gl.getUniformLocation(program, "iTime");
+
+  // Create a vertex array object (attribute state)
+  const vao = gl.createVertexArray();
+
+  // and make it the one we're currently working with
+  gl.bindVertexArray(vao);
+
+  // Create a buffer to put three 2d clip space points in
+  const positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // fill it with a 2 triangles that cover clip space
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1, -1,  // first triangle
+    1, -1,
+    -1, 1,
+    -1, 1,  // second triangle
+    1, -1,
+    1, 1,
+  ]), gl.STATIC_DRAW);
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    2,          // 2 components per iteration
+    gl.FLOAT,   // the data is 32bit floats
+    false,      // don't normalize the data
+    0,          // 0 = move forward size * sizeof(type) each iteration to get the next position
+    0,          // start at the beginning of the buffer
+  );
+
+  let mouseX = 0;
+  let mouseY = 0;
+
+  function setMousePosition(e) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = e.clientX - rect.left;
+    mouseY = rect.height - (e.clientY - rect.top) - 1;  // bottom is 0 in WebGL
+  }
+
+  canvas.addEventListener('mousemove', setMousePosition);
+  canvas.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+  }, { passive: false });
+  canvas.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    setMousePosition(e.touches[0]);
+  }, { passive: false });
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao);
+
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+    gl.uniform2f(mouseLocation, mouseX, mouseY);
+    gl.uniform1f(timeLocation, time);
+
+    gl.drawArrays(
+      gl.TRIANGLES,
+      0,     // offset
+      6,     // num vertices to process
+    );
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+main();
